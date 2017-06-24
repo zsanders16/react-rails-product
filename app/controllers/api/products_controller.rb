e05dceecdebf057dea3d7d6e698d487842b73985
Zachary Sanders <zsanders@icloud.com>
2017-06-24 14:13:27 -0600

    create rails api

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -6,25 +6,35 @@ class Api::ProductsController < ApplicationController
   end
 
   def create
-
+    product = Product.new
+    if product.save
+      render json: product
+    else
+      render_error(product)
+    end
   end
 
   def update
-
+    if @product.update(product_params[:id])
+      render json: @product
+    else
+      render_error(@product)
+    end
   end
 
   def destroy
-
+    @product.destroy
+    render json: { message: 'Product deleted'}
   end
 
   private
 
     def product_params
-
+      params.require(:product).permit(:name, :description, :price)
     end
 
     def set_product
-
+      @product = Product.find(params[:id])
     end
 
 end
